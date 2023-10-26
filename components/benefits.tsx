a1af6e6d6ee09825f73555d61f3d106b2a922123
joel danjuma <leojjad@gmail.com>
2023-10-26 19:32:33 +0100

    Updated Files

--- components/benefits.tsx
+++ components/benefits.tsx
@@ -31,7 +31,7 @@ const features: FeatureType[] = [
 
 const Benefits = () => {
   return (
-    <section id="benefits" className="w-full h-full p-4 bg-gray-200 )">
+    <section id="benefits" className="w-full h-full p-3 bg-gray-200 )">
       <SectionTitle title="We don't try to reinvent the wheel We Just develop Imaginatively">
         Design as you know it is out the door. Design as you want it just
         arrived.
