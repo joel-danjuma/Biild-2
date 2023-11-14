63a663d379d5a20715492b3030f0b00acc0c5e4e
joel danjuma <leojjad@gmail.com>
2023-11-14 10:27:35 +0100

    Updated Files

--- components/motion/animateText.tsx
+++ components/motion/animateText.tsx
@@ -6,12 +6,15 @@ import { useRef } from "react";
 const defaultAnimations = {
   hidden: {
     opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
   },
   visible: {
     opacity: 1,
-  },
-  transition: {
-    duration: 1,
+    transition: {
+      duration: 0.5,
+    },
   },
 };
 
@@ -21,28 +24,30 @@ const AnimateText = ({
   className,
 }: AnimatedTextProps) => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { amount: 0.5, once: true });
+  const isInView = useInView(ref, { amount: 0.5 });
 
   return (
     <Wrapper className={className}>
       <span className="sr-only">{text}</span>
       <motion.span
         ref={ref}
+        // variants={defaultAnimations}
         initial="hidden"
-        // animate={isInView ? "visible " : "hidden"}
-        animate="visible"
+        animate={isInView ? "visible" : "hidden"}
+        // initial="hidden"
+        // animate="visible"
         transition={{ staggerChildren: 0.1 }}
         aria-hidden
       >
-        {text.split("").map((char: string, i: number) => (
-          <motion.span
-            key={i}
-            variants={defaultAnimations}
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-          >
-            {char}
-          </motion.span>
+        {text.split(" ").map((word: string, i: number) => (
+          <span key={i} className="inline-block">
+            {word.split(" ").map((char: string, i: number) => (
+              <motion.span key={i} variants={defaultAnimations}>
+                {char}
+              </motion.span>
+            ))}
+            <span className="inline-block">&nbsp;</span>
+          </span>
         ))}
       </motion.span>
     </Wrapper>
